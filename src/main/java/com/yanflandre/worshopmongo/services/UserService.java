3633c1fdd51b57cee8e8d3c6b978a9cae8ce0021
YanFlandre <jp_felippe@hotmail.com>
2024-07-15 11:08:03 -0300

    Obtaining an user by ID

--- src/main/java/com/yanflandre/worshopmongo/services/UserService.java
+++ src/main/java/com/yanflandre/worshopmongo/services/UserService.java
@@ -1,22 +1,29 @@
 package com.yanflandre.worshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.yanflandre.worshopmongo.domain.User;
 import com.yanflandre.worshopmongo.repository.UserRepository;
+import com.yanflandre.worshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
-		
+
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
