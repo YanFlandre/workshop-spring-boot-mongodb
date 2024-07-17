b017143d3fe8c761f30c07c8b2835be47f7360fe
YanFlandre <jp_felippe@hotmail.com>
2024-07-17 13:49:28 -0300

    Endpoint for returning posts from user

--- src/main/java/com/yanflandre/worshopmongo/resources/UserResource.java
+++ src/main/java/com/yanflandre/worshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.yanflandre.worshopmongo.domain.Post;
 import com.yanflandre.worshopmongo.domain.User;
 import com.yanflandre.worshopmongo.dto.UserDTO;
 import com.yanflandre.worshopmongo.services.UserService;
@@ -57,6 +58,12 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
